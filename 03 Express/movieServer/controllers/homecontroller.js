53864768836eec3f4417f181ca2fe438f9360e09
Vartan K. <vartankebedjian@gmail.com>
2026-05-10 12:44:00 +0300

    Add files via upload

---
+++ 03 Express/movieServer/controllers/homecontroller.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const homeController = express.Router();
+const movieService = require('../services/movieService');
+
+homeController.get('/', (request, response) => {
+    const movies = movieService.readMovies();
+
+    response.render('home', { movies });
+});
+
+homeController.get('/about', (request, response) => {
+    response.render('about');
+});
+
+homeController.get('/search', (request, response) => {
+    const { title, genre, year } = request.query;
+
+    const movies = movieService.search(title, genre, year);
+    response.render('search', { movies });
+});
+
+module.exports = homeController;
